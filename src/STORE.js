5ada6943aa1fa93c2522c326cc388848fbc7289a
William Y Wong <wwong1987@gmail.com>
2020-02-17 22:56:35 -0800

    completed challenge 1 of displaying data with jsx

---
+++ src/STORE.js
@@ -0,0 +1,14 @@
+
+// Data set for React Challenge #1
+const Users = {
+  name: 'William Wong',
+  location: 'Las Vegas',
+  foodType: 'Everything',
+  age: ':)',
+  likes: 'Coding into the wee hours of the morning',
+  twitterUsername: 'dc5will',
+  avatar:
+    'https://avatars0.githubusercontent.com/u/43019223?s=460&v=4'
+};
+
+export default Users;
