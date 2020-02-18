056c0ebd4170c8726a33d95495d35740b2f41049
William Y Wong <wwong1987@gmail.com>
2020-02-17 23:26:55 -0800

    refactored UserProfile into a separate component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
 import React from 'react';
-import Users from '../src/STORE';
+import UserProfile from '../src/components/UserProfile/UserProfile';
 import './App.css';
 
 function App() {
-  const url = `http://twitter.com/@${Users.twitterUsername}`
-
-
   return (
     <div className="App">
       {/* Show user data here */}
-      <div className="user-deets">
-        <img src={url} alt="user avatar" />
-
-        <h3>
-          <a href="https://github.com/dc5will">{Users.name}</a>
-        </h3>
-
-        <p>
-          <strong>Location</strong>
-          {Users.location}
-        </p>
-
-        <p>
-          <strong>Eats</strong>
-          {Users.foodType}
-        </p>
-
-        <p>
-          <strong>Age</strong>
-          {Users.age}
-        </p>
-
-        <p>
-          <strong>Likes</strong>
-          {Users.likes}
-        </p>
-
-        <strong>Twitter</strong>
-        <a href={url}>@{Users.twitterUsername}</a>
-      </div>
+      <UserProfile/>
     </div>
   );
 }
---
+++ src/components/UserProfile/UserProfile.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Users from '../../STORE';
+
+export default function UserProfile(props) {
+  const url = `http://twitter.com/@${Users.twitterUsername}`
+
+  return (
+    <div className="user-deets">
+        <img src={Users.avatar} alt="user avatar" />
+
+        <h3>
+          <a href="https://github.com/dc5will">{Users.name}</a>
+        </h3>
+
+        <p>
+          <strong>Location</strong>
+          {Users.location}
+        </p>
+
+        <p>
+          <strong>Eats</strong>
+          {Users.foodType}
+        </p>
+
+        <p>
+          <strong>Age</strong>
+          {Users.age}
+        </p>
+
+        <p>
+          <strong>Likes</strong>
+          {Users.likes}
+        </p>
+
+        <strong>Twitter</strong>
+        <a href={url}>@{Users.twitterUsername}</a>
+      </div>
+  )
+}
+
