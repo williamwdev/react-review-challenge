6e34eace7aa4aec26c3dcad639a5e29ee9ad2725
William Y Wong <wwong1987@gmail.com>
2020-02-18 16:27:38 -0800

    completed challenge 5 of using calculator to add two numbers

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 /** The Challenge: Add Two Numbers
  * Store input numbers
@@ -7,18 +7,36 @@ import React from 'react';
  */
 
 export default function Calculator(props) {
+  const [number1, setNumber1] = useState();
+  const [number2, setNumber2] = useState();
+  const [sum, setSum] = useState(number1 + number2);
+
+  function calculateTotal() {
+    setSum(number1 + number2);
+  }
+
   return (
     <div className="calculator-container">
       <h1>Adding Two Numbers</h1>
 
       <div className="number-inputs">
-        <input type="number" placeholder="0" />
-        <input type="number" placeholder="0" />
+        <input
+          type="number"
+          placeholder="0"
+          value={number1}
+          onChange={e => setNumber1(+e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="0"
+          value={number2}
+          onChange={e => setNumber2(+e.target.value)}
+        />
       </div>
 
-      <button>Add Them!</button>
+      <button onClick={calculateTotal}>Add Them!</button>
 
-      <h2>0</h2>
+      <h2>{sum}</h2>
     </div>
   );
 }
