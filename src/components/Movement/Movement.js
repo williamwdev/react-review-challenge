46ac069e29b805d04babee90d5a5911555680d64
William Y Wong <wwong1987@gmail.com>
2020-02-18 17:24:07 -0800

    completed basic functionality of moving the box up

--- src/components/Movement/Movement.js
+++ src/components/Movement/Movement.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 /** The Challenge: Move a Box Up
  * Create a state variable to hold the value of the transformation points
@@ -7,15 +7,24 @@ import React from 'react';
  */
 
 export default function Movement() {
+  const [yAxis, setYAxis] = useState(300);
+
+  function moveBoxUp() {
+    setYAxis(yAxis - 50);
+  }
+
   return (
+    
     <div className="movement-container">
       <h1>Move the Box!</h1>
 
       {/* handle the click event on this button */}
-      <button>👆 Move Up 👆</button>
+      <button onClick={moveBoxUp}>👆 Move Up 👆</button>
 
       {/* move this box using inline styles */}
-      <div className="box" style={{}} />
+      <div className="box" style={{
+        transform: `translateY(${yAxis}px)`
+      }} />
     </div>
   );
 }
